9bdf792498a0cec62ee2781471cc82ce503b2dd7
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-14 11:49:50 -0700

    wip

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.use('*', (req, res) => {
+    res.send(`<h1>Hello world!</h1>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message, stack: err.stack })
+})
+
+server.get('/', (req, res) => {
+    res.json({ message: 'api working' });
+})
+
+module.exports = server;
